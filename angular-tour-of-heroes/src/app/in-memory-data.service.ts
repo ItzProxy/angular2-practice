99c83aeb4f702332c053c7eaa6cff387d88befae
ItzProxy <allure.success@gmail.com>
2018-05-13 16:19:06 -0600

    As you can tell, am very bad at this

---
+++ angular-tour-of-heroes/src/app/in-memory-data.service.ts
@@ -0,0 +1,26 @@
+import { InMemoryDbService } from 'angular-in-memory-web-api';
+
+export class InMemoryDataService implements InMemoryDbService {
+  createDb() {
+    const heroes = [
+        { id: 0, name: 'Ben Brode', heroPower: 'Laugh'},
+        { id: 11, name: 'Malfurion Stormrage', heroPower: 'Shapeshifter'},
+        { id: 12, name: 'Lunara', heroPower: 'Shapeshifter'},
+        { id: 13, name: 'Rexxar', heroPower: 'Shapeshifter' },
+        { id: 14, name: 'Alleria Windrunner', heroPower: 'Shapeshifter' },
+        { id: 15, name: 'Jaina Proudmoore', heroPower: 'Shapeshifter' },
+        { id: 16, name: 'Khadgar', heroPower: 'Shapeshifter' },
+        { id: 17, name: 'Medivh', heroPower: 'Shapeshifter' },
+        { id: 18, name: 'Uther Lightbringer', heroPower: 'Shapeshifter' },
+        { id: 19, name: 'Lady Liadrin', heroPower: 'Shapeshifter' },
+        { id: 20, name: 'Prince Arthas', heroPower: 'Shapeshifter' },
+        { id: 21, name: 'Anduin Wrynn', heroPower: 'Shapeshifter'},
+        { id: 22, name: 'Tyrande Whisperwind', heroPower: 'Shapeshifter'},
+        { id: 23, name: 'Valeera Sanguinar', heroPower: 'Shapeshifter'},
+        { id: 24, name: 'Maiev Shadowsong', heroPower: 'Shapeshifter'},
+        { id: 25, name: 'Gul dan', heroPower: 'Shapeshifter'},
+        { id: 26, name: 'Garrosh Hellscream', heroPower: 'Shapeshifter'}
+    ];
+    return {heroes};
+  }
+}
