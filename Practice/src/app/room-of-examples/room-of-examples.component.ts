db5e3c73a14571fd10bc1a0c0a12bb7c5d8ce6a3
ItzProxy <allure.success@gmail.com>
2018-05-13 15:52:29 -0600

    testing out component binding, routing, and using a memory based database to handle certain tasks

---
+++ Practice/src/app/room-of-examples/room-of-examples.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-room-of-examples',
+  templateUrl: './room-of-examples.component.html',
+  styleUrls: ['./room-of-examples.component.css']
+})
+export class RoomOfExamplesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
